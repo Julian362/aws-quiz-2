42ce8fd7352b41545e35ce9bf2bbe0337f32704c
Julian362 <54406277+Julian362@users.noreply.github.com>
2024-09-21 15:27:50 +0000

    udpate server.py

--- list/src/server.py
+++ list/src/server.py
@@ -7,15 +7,22 @@ server = Flask(__name__)
 s3 = boto3.client('s3')
 
 @server.route("/buckets", methods=['GET'])
-def list_buckets():
+def list_objects():
+    bucket_name = request.args.get('name')
+    
+    if not bucket_name:
+        return jsonify({"error": "Bucket name is required"}), 400
+
     try:
-        response = s3.list_buckets()
-        buckets = response['Buckets']
-        buckets_names = [bucket['Name'] for bucket in buckets]
-        return jsonify(buckets_names)
+        response = s3.list_objects_v2(Bucket=bucket_name)
+        if 'Contents' in response:
+            objects = [obj['Key'] for obj in response['Contents']]
+            return jsonify({"bucket": bucket_name, "objects": objects})
+        else:
+            return jsonify({"bucket": bucket_name, "objects": []})  # No objects in the bucket
 
     except ClientError as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0')
